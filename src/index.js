9fb95d466e7d4b436e69ec3ff3b29da2bb9cfc49
zsj <cho010105@naver.com>
2025-04-07 21:01:32 +0900

    [ADD] Add required library & Sentry SDK

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+    dsn: "https://827f0d242113917609e71527db4fd2da@o4509111730634752.ingest.us.sentry.io/4509111732142080",
+    integrations: [new BrowserTracing()],
+    tracesSampleRate: 1.0, // 퍼포먼스 트래킹 비율 (0.0 ~ 1.0)
+    environment: "development", // 또는 "production"
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <Sentry.ErrorBoundary fallback={<p>Something went wrong</p>}>
+            <App />
+        </Sentry.ErrorBoundary>
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
