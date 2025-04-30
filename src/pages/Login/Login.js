483816034518a5c0a8de5e0091683b4527297f21
zsj <cho010105@naver.com>
2025-04-30 14:19:29 +0900

    [FEATURE] 로그인 카카오 URL 임시연동 1. 현재 카카오 로그인 페이지로는 이동이 가능하나, 로그인 이후 redirect는 400 에러 발생.(백엔드에서 처리중)

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -33,9 +33,10 @@ function Login() {
                     <p className={styles.login_subtitle}>춘삼이와 함께 새로운 여정을 시작해 볼까요?</p>
                 </div>
 
-                {/* 링크 나중에 수정 */}
-                <a href="/choice-team" className={`${styles.login_button} ${styles.kakao_button}`}>
-                {/*<a href="/choice-team" className={`${styles.login_button} ${styles.kakao_button}`} onClick={onButtonClick}>*/}
+                <a
+                    href="https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=e293c26c0c0476eb1377b380a3b0230d&redirect_uri=http://localhost:3000/auth/kakao/callback"
+                    className={`${styles.login_button} ${styles.kakao_button}`}
+                >
                     <img
                         src={`${process.env.PUBLIC_URL}/Logo/KaKao_logo.png`}
                         alt="Kakao Icon"
