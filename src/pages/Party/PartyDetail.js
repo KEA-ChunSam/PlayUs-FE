f8ec01cb82b8427cc840172285896612a8653f14
zsj <cho010105@naver.com>
2025-05-06 15:48:51 +0900

    [FIX] useState 상태 사용 방식 수정 1. activeTab 상태 선언 수정

--- src/pages/Party/PartyDetail.js
+++ src/pages/Party/PartyDetail.js
@@ -7,7 +7,7 @@ import TabNav from "../../components/TabNav/TabNav";
 const PartyDetail = () => {
     // const {partyId} = useParams();
     const navigate = useNavigate();
-    const [setActiveTab] = useState(0);
+    const [activeTab, setActiveTab] = useState(0);
     const tabLabels = ["직관팟 구하기", "내 신청 현황", "승인 요청"];
     function applyParty() {
         navigate("/Party/applyParty/partyid");
