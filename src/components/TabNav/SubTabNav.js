22e9955409cba0cd89d84d081fa389b6b8a4cc5f
zsj <cho010105@naver.com>
2025-05-08 23:11:37 +0900

    [FIX] 오탈자 수정 1. SubTabNav.js 오타 제거

--- src/components/TabNav/SubTabNav.js
+++ src/components/TabNav/SubTabNav.js
@@ -30,7 +30,7 @@ const SubTabNav = ({tabs = [], onTabChange}) => {
                         role="tab"
                         id={`tab-${index}`}
                         aria-selected={activeIndex === index}
-                        aria-controls={`tabpanel-${index}`} ㅡ
+                        aria-controls={`tabpanel-${index}`}
                         tabIndex={activeIndex === index ? 0 : -1}
                         onKeyDown={(e) => {
                             if (e.key === 'Enter' || e.key === ' ') {
