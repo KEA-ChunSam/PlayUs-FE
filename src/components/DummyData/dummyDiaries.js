8fd56fcf283f6fbf635460c7316161e5c46926e5
zsj <cho010105@naver.com>
2025-05-12 22:16:06 +0900

    [FEATURE] 직관일지 데이터 달력 표시 1. 직관일지 데이터를 Calendar와 연동하여 프로필 메인 페이지에 표시 2. userId에 따른 프로필 표시 내용 변화 적용: 상대방 프로필의 경우 '직관 타율', 직관일지 작성 횟수, 가입일 및 커뮤니티 게시글 내역이 나타남

--- src/components/DummyData/dummyDiaries.js
+++ src/components/DummyData/dummyDiaries.js
@@ -23,6 +23,7 @@ const dummyDiaries = [
   {
     id: 3,
     title: 'KT 위즈 경기 첫 관람!',
+    team: 'KT 위즈',
     date: '2025.05.05',
     image: `${process.env.PUBLIC_URL}/exImage.png`,
     teamLogo: `${process.env.PUBLIC_URL}/Logo/TeamLogo/emblem_SS.png`,
@@ -30,6 +31,7 @@ const dummyDiaries = [
   {
     id: 4,
     title: '친구와 함께한 경기장 나들이',
+    team: 'SSG 랜더스',
     date: '2025.03.17',
     image: `${process.env.PUBLIC_URL}/exImage.png`,
     teamLogo: `${process.env.PUBLIC_URL}/Logo/TeamLogo/emblem_SK.png`,
@@ -37,6 +39,7 @@ const dummyDiaries = [
   {
     id: 5,
     title: '우천취소라 아쉬웠던 하루',
+    team: '두산 베어스',
     date: '2025.03.18',
     image: `${process.env.PUBLIC_URL}/exImage.png`,
     teamLogo: `${process.env.PUBLIC_URL}/Logo/TeamLogo/emblem_OB.png`,
@@ -44,6 +47,7 @@ const dummyDiaries = [
   {
     id: 6,
     title: '경기 전 응원단 구경이 제일 재밌었음',
+    team: '키움 히어로즈',
     date: '2025.03.19',
     image: `${process.env.PUBLIC_URL}/exImage.png`,
     teamLogo: `${process.env.PUBLIC_URL}/Logo/TeamLogo/emblem_WO.png`,
@@ -51,6 +55,7 @@ const dummyDiaries = [
   {
     id: 7,
     title: '간식먹으러 야구장 간 썰',
+    team: 'LG 트윈스',
     date: '2025.03.02',
     image: `${process.env.PUBLIC_URL}/exImage.png`,
     teamLogo: `${process.env.PUBLIC_URL}/Logo/TeamLogo/emblem_LG.png`,
@@ -58,6 +63,7 @@ const dummyDiaries = [
   {
     id: 8,
     title: '홈런 두 방! 타격전 직관기',
+    team: '롯데 자이언츠',
     date: '2025.03.03',
     image: `${process.env.PUBLIC_URL}/exImage.png`,
     teamLogo: `${process.env.PUBLIC_URL}/Logo/TeamLogo/emblem_LT.png`,
@@ -65,6 +71,7 @@ const dummyDiaries = [
   {
     id: 9,
     title: '시구 보러 갔다가 경기 매력에 빠짐',
+    team: 'KT 위즈',
     date: '2025.03.04',
     image: `${process.env.PUBLIC_URL}/exImage.png`,
     teamLogo: `${process.env.PUBLIC_URL}/Logo/TeamLogo/emblem_KT.png`,
@@ -72,6 +79,7 @@ const dummyDiaries = [
   {
     id: 10,
     title: '부모님과 함께한 가족 직관',
+    team: 'NC 다이노스',
     date: '2025.03.20',
     image: `${process.env.PUBLIC_URL}/exImage.png`,
     teamLogo: `${process.env.PUBLIC_URL}/Logo/TeamLogo/emblem_NC.png`,
