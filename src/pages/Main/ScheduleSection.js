56cb196bfe11ce6f3ab1d7910019aae307d4b339
zsj <cho010105@naver.com>
2025-05-22 19:18:49 +0900

    [FIX] 방어코드 추가 1. .gitignore에 idea 폴더 추가 2. 팀 로고 이미지 예외처리 추가

--- src/pages/Main/ScheduleSection.js
+++ src/pages/Main/ScheduleSection.js
@@ -30,13 +30,13 @@ export default function ScheduleSection({ schedule }) {
           <div className={styles.name}>{away}</div>
           <div className={styles.player}>오승환</div>
         </div>
-        <img src={`${process.env.PUBLIC_URL}/Logo/TeamLogo/${teamLogoMap[away]}`} alt={away} className={styles.logo} />
+        <img src={`${process.env.PUBLIC_URL}/Logo/TeamLogo/${teamLogoMap[away] || 'emblem_default.png'}`} alt={away} className={styles.logo} />
         <div className={styles.score}>{score[1]}</div>
 
         <div className={styles.status}>{status}</div>
 
         <div className={styles.score}>{score[0]}</div>
-        <img src={`${process.env.PUBLIC_URL}/Logo/TeamLogo/${teamLogoMap[home]}`} alt={home} className={styles.logo} />
+        <img src={`${process.env.PUBLIC_URL}/Logo/TeamLogo/${teamLogoMap[home] || 'emblem_default.png'}`} alt={home} className={styles.logo} />
         <div className={styles.teamColumn}>
           <div className={styles.name}>{home}</div>
           <div className={styles.player}>우강훈</div>
