d0cf7f867681b4a2a0da437f24d50992a5c8aea1
zsj <cho010105@naver.com>
2025-06-08 20:28:00 +0900

    [HOTFIX] test 코드 충돌 수정

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,6 @@
 import { render, screen } from '@testing-library/react';
 import AppRouter from './routes/AppRouter';
 import React from 'react';
-import { HashRouter } from 'react-router-dom';
 
 describe('AppRouter', () => {
   test('스플래시 페이지가 기본 경로(/)에서 렌더링됩니다', () => {
