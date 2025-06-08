faf899572e82e725a232d668b2b674674935036d
zsj <cho010105@naver.com>
2025-06-08 15:47:43 +0900

    [DESIGN] 챗봇 헤더 고정 1. 챗봇 메시지가 특정 height 이상으로 늘어나더라도 header도 같이 스크롤되지 않도록 수정

--- src/pages/Chatbot/CasterbotModal.js
+++ src/pages/Chatbot/CasterbotModal.js
@@ -9,19 +9,7 @@ const CasterbotModal = ({onClose}) => {
             content: '안녕하세요! 오늘은 KBO에 대해 무엇이 궁금하신가요?',
             // time: '11:30 AM',
             mine: false,
-        },
-        // {
-        //     sender: '',
-        //     content: '현재 타율 1위가 누군지 알려줘',
-        //     // time: '11:30 AM',
-        //     mine: true,
-        // },
-        // {
-        //     sender: '캐스터봇',
-        //     content: '2025시즌 타율 1위는 NC 다이노스의 손아섭 선수입니다.',
-        //     // time: '11:30 AM',
-        //     mine: false,
-        // }
+        }
     ]);
 
     const [chatInput, setChatInput] = useState('');
@@ -87,33 +75,35 @@ const CasterbotModal = ({onClose}) => {
                                                        className={styles.headerImg}/> 캐스터봇
                     </div>
                 </header>
-                <div className={styles.chatBody}>
-                    {messages.map((msg, i) => (
-                        <div key={i} className={msg.mine ? styles.myMessage : styles.botMessage}>
-                            {!msg.mine && (
-                                <div className={styles.avatarWrapper}>
-                                    <img src={`${process.env.PUBLIC_URL}/Button/casterbot.png`} alt="bot"
-                                         className={styles.avatar}/>
+                <div className={styles.modalBodyScrollable}>
+                    <div className={styles.chatBody}>
+                        {messages.map((msg, i) => (
+                            <div key={i} className={msg.mine ? styles.myMessage : styles.botMessage}>
+                                {!msg.mine && (
+                                    <div className={styles.avatarWrapper}>
+                                        <img src={`${process.env.PUBLIC_URL}/Button/casterbot.png`} alt="bot"
+                                             className={styles.avatar}/>
+                                    </div>
+                                )}
+                                <div className={styles.messageContent}>
+                                    {!msg.mine && <div className={styles.sender}>{msg.sender}</div>}
+                                    <div className={styles.messageBubble}>{msg.content}</div>
+                                    {/*<div className={styles.timestamp}>{msg.time}</div>*/}
                                 </div>
-                            )}
-                            <div className={styles.messageContent}>
-                                {!msg.mine && <div className={styles.sender}>{msg.sender}</div>}
-                                <div className={styles.messageBubble}>{msg.content}</div>
-                                {/*<div className={styles.timestamp}>{msg.time}</div>*/}
                             </div>
-                        </div>
-                    ))}
-                </div>
-                <div className={styles.inputArea}>
-                    {/*<button className={styles.plusBtn}>+</button>*/}
-                    <input
-                        className={styles.chatInput}
-                        placeholder="메세지를 입력하세요."
-                        value={chatInput}
-                        onChange={(e) => setChatInput(e.target.value)}
-                        onKeyDown={handleKeyDown}
-                    />
-                    <button className={styles.sendBtn} onClick={handleSendMessage}>➤</button>
+                        ))}
+                    </div>
+                    <div className={styles.inputArea}>
+                        {/*<button className={styles.plusBtn}>+</button>*/}
+                        <input
+                            className={styles.chatInput}
+                            placeholder="메세지를 입력하세요."
+                            value={chatInput}
+                            onChange={(e) => setChatInput(e.target.value)}
+                            onKeyDown={handleKeyDown}
+                        />
+                        <button className={styles.sendBtn} onClick={handleSendMessage}>➤</button>
+                    </div>
                 </div>
             </div>
         </div>
